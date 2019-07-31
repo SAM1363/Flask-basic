c6574dd8308be898bbc3f7ae7d5d170e83f6819a
Samson <isamu3636136@gmail.com>
2019-07-30 23:28:51 -0500

    flask social app, create login logout register

--- Social/models.py
+++ Social/models.py
@@ -1,7 +1,10 @@
 import datetime
+
+from flask＿bcrypt import generate_password_hash
+from flask_login import UserMixin
 from peewee import *
-from flask.ext.login import UserMixin
-from flask.ext.bcrypt import generate_password_hash
+
+DATABASE = SqliteDatabase('social.db')
 
 class User(UserMixin, Model):
     username = CharField(unique=True)
@@ -9,22 +12,24 @@ class User(UserMixin, Model):
     password = CharField(max_length=100)
     joined_at = DateTimeField(default=datetime.datetime.now)
     is_admin = BooleanField(default=False)
-    bio = TextField(default='')
-
-
+    
     class Meta:
-        database = Database
+        database = DATABASE
         order_by = ('-joined_at',)
-
-
+        
     @classmethod
     def create_user(cls, username, email, password, admin=False):
         try:
             cls.create(
-                username = username,
-                email = email,
-                password = generate_password_hash(password),
-                is_admin = admin
-            )
+                username=username,
+                email=email,
+                password=generate_password_hash(password),
+                is_admin=admin)
         except IntegrityError:
-            raise ValueError('already taken')
+            raise ValueError("User already exists")
+            
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([User], safe=True)
+    DATABASE.close()
